6e7932307a57240f5b4b93fab970f67f6f5989bf
Absolutestunna <joshujah007@gmail.com>
2017-01-17 19:07:37 -0500

    check in range

---
+++ Python-Bootcamp/Functions-Method-Homework/check-in-range.py
@@ -0,0 +1,9 @@
+
+def ran_bool(num,low,high):
+
+    '''
+        Write a function that checks whether a number is in a given range (Inclusive of high and low)
+    '''
+    return num in (range(low, high))
+
+ran_bool(3,1,10)
