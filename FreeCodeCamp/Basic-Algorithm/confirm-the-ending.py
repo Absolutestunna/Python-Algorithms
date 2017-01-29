06d1a57a1a9a53195c75f687c84e3e68fb0a0525
Absolutestunna <joshujah007@gmail.com>
2017-01-28 19:59:58 -0500

    made new dirs and redid confir-the-ending

--- FreeCodeCamp/Basic-Algorithm/confirm-the-ending.py
+++ FreeCodeCamp/Basic-Algorithm/confirm-the-ending.py
@@ -2,21 +2,21 @@ def confirmEnding(str, target):
 
     '''
         Assignment:
-
+        
             https://www.freecodecamp.com/challenges/confirm-the-ending
-
-            Check if a string (first argument, str) ends with the given target string (second argument, target).
-
-        Special notes:
-            in is used to check if target argument is a substring of str argument
+            Check True if a string (first argument, str) ends with the given target string (second argument, target).
 
     '''
 
-    return target in str
+    split_str = str.split();               #get a new list
 
+    if len(split_str) > 1:                 # check if the list is > 1
+        last_word = split_str.pop()        # get the last word
+        return last_word == target         # return a bool after evaluation
+    else:
+        i_value = str[len(str) - 1]        # get the last word
+        return i_value == target           # return a bool after evaluation
 
+confirmEnding("Walking", "f")                 #should return False
 
-
-confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")
-
-confirmEnding("Walking", "g")
+confirmEnding("Walking in the rain", "rain")  #should return True
