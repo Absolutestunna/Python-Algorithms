923039840033a89f772134d8d98f5ccca8439ec6
Absolutestunna <joshujah007@gmail.com>
2017-02-03 19:15:28 -0500

    second answer for problem 3

--- Python-Bootcamp/error-exceptions/problem-2.py
+++ Python-Bootcamp/error-exceptions/problem-2.py
@@ -1,11 +1,10 @@
 # Problem 2
 # Handle the exception thrown by the code below by using try and except blocks. Then use a finally block to print 'All Done.'
 
+x = 5
+y = 0
 
 try:
-    x = 5
-    y = 0
-
     z = x/y
 except:
     print('cannot divide by zero')
--- Python-Bootcamp/error-exceptions/problem-3.py
+++ Python-Bootcamp/error-exceptions/problem-3.py
@@ -2,15 +2,18 @@
 # Write a function that asks for an integer and prints the square of it. Use a while loop with a try,except, else block to account for incorrect inputs.
 
 def ask():
-    try:
-        val = int(input('Please enter an integer'))
-    except:
-        print('An error occurred! Please try again!')
-        continue
-    else:
-        break
 
-    return print('Thank you, your number squared is: ', val ** 2)
+    while True:
+        try:
+            n = input('Input an integer: ')
+        except:
+            print('An error occurred! Please try again!')
+            continue
+        else:
+            break
+
+
+    print('Thank you, you number squared is: ', n**2)
 
 
 ask()
