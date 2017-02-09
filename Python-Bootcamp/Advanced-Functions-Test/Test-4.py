0813b59dbcdeeadfb0e96679136cb88940388526
Absolutestunna <joshujah007@gmail.com>
2017-02-09 17:32:30 -0500

    test4

---
+++ Python-Bootcamp/Advanced-Functions-Test/Test-4.py
@@ -0,0 +1,9 @@
+
+def concatenate(L1, L2, connector):
+    '''
+        Use zip and list comprehension to return a list of the same length where each value is the two strings from L1 and L2 concatenated together with connector between them. Look at the example output below:
+    '''
+
+
+
+concatenate(['A','B'],['a','b'],'-') #['A-a', 'B-b']
