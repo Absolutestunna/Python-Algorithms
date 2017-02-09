625ae10e83f82c1fc67e58ecdc7dfe0d1e602fe5
Absolutestunna <joshujah007@gmail.com>
2017-02-09 17:35:16 -0500

    test4

--- Python-Bootcamp/Advanced-Functions-Test/Test-4.py
+++ Python-Bootcamp/Advanced-Functions-Test/Test-4.py
@@ -1,8 +1,14 @@
-
 def concatenate(L1, L2, connector):
     '''
-        Use zip and list comprehension to return a list of the same length where each value is the two strings from L1 and L2 concatenated together with connector between them. Look at the example output below:
+        Use zip and list comprehension to return a list of the
+        same length where each value is the two strings
+        from L1 and L2 concatenated together with connector
+        between them.
     '''
+
+    zipped_tuple = list(zip(L1, L2))
+    z2 = ['-'.join(result) for result in zipped_tuple]
+    return z2
 
 
 
