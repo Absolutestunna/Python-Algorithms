6f586f60b74184d166fa09bbbc54d524a651629b
Absolutestunna <joshujah007@gmail.com>
2017-02-10 15:48:36 -0500

    test-2

--- Python-Bootcamp/Advanced-Functions-Test/Test-2.py
+++ Python-Bootcamp/Advanced-Functions-Test/Test-2.py
@@ -1,9 +1,10 @@
+from functools import reduce
 
 def digits_to_num(digits):
     '''
         Use reduce to take a list of digits and return the number that they correspond to. Do not convert the integers to strings!
 
     '''
-    reduce(lambda x: x, digits)
+    return reduce(lambda x,y: x*10 + y,digits)
 
 digits_to_num([3,4,3,2,1]) #output 34321
